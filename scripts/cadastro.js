65d06ebb83c3d80610a07db0cf3b94dc58e2a6b2
Willian de Oliveira Ribeiro <123466324+oliver-will61@users.noreply.github.com>
2023-11-10 09:37:07 -0300

    Add files via upload

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,13 +1,51 @@
 
-const radio = document.querySelectorAll('.radio')
+const radio = document.querySelectorAll('.radio') //selecionar todos radios
+const containerFormularioCadastroJs = document.querySelector('.container-formulario-cadastro-js')
 
-function obterValorRadio() {
+function obterValorRadio() { //obtém o valor que está no radio selecionado
     const entradaRadio = document.querySelector('.radio[name="opcao"]:checked');
-    console.log(entradaRadio.value)
+    return entradaRadio.value
 }
 
-for(i=0; i <= radio.length -1; i++){
-    radio[i].addEventListener('click', obterValorRadio);
+//main ================================================================================================================================================================
+
+for(i=0; i <= radio.length -1; i++){ //carrega todos os radios e para cada um chama a função "obterValorRadio"
+    
+    radio[i].addEventListener('click', function() {
+        entradaRadioValor = obterValorRadio();
+
+        if (entradaRadioValor== 'Cliente'){
+            containerFormularioCadastroJs.innerHTML = `
+                <label for="nome-cadastro">Nome e Sobrenome
+                    <input id="nome-cadastro" class="formulario-cadastro-entrada" type="text" >
+                </label>
+
+                <label for="cpf-cadastro">CPF
+                    <input id="cpf-cadastro" class="formulario-cadastro-entrada" type="number" >
+                </label>`
+
+        }
+
+        else if (entradaRadioValor == 'Estabelecimento'){
+            containerFormularioCadastroJs.innerHTML = `
+                <label for="nomo-empresa-cadastro">Nome da Empresa
+                    <input id="nomo-empresa-cadastro" class="formulario-cadastro-entrada" type="text" >
+                </label>
+                
+                <label for="cnpj-cadastro">CNPJ
+                    <input id="cnpj-cadastro" class="formulario-cadastro-entrada" type="text" >
+                </label>`
+        }
+    }); 
 }
+
+
+
+
+
+
+
+
+
 
 
