13128d492848c0cdc7af62482a9153e5f31a351a
Willian de Oliveira Ribeiro <123466324+oliver-will61@users.noreply.github.com>
2023-11-11 23:54:47 -0300

    Add files via upload

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,6 +1,8 @@
 
 const radio = document.querySelectorAll('.radio') //selecionar todos radios
 const containerFormularioCadastroJs = document.querySelector('.container-formulario-cadastro-js')
+const botao = document.querySelector('.botao')
+
 
 function obterValorRadio() { //obtém o valor que está no radio selecionado
     const entradaRadio = document.querySelector('.radio[name="opcao"]:checked');
@@ -34,10 +36,38 @@ for(i=0; i <= radio.length -1; i++){ //carrega todos os radios e para cada um ch
                 
                 <label for="cnpj-cadastro">CNPJ
                     <input id="cnpj-cadastro" class="formulario-cadastro-entrada" type="text" >
-                </label>`
+                </label>
+                
+                <label for="cep-cadastro">CEP
+                    <input id="cep-cadastro" class="formulario-cadastro-entrada" type="text" >
+                </label>`   
         }
     }); 
 }
+
+botao.addEventListener('click', function (){
+
+    const senhaInput = document.querySelectorAll('#senha-cadastro')  // pega todos os inputs de senha
+    const senhaInputTexto = document.querySelector('#senha-cadastro-texto')  //tag span que aparece em caso de erro
+
+    let senhaInputValor = senhaInput[0].value
+    let senhaInputValor2 = senhaInput[1].value
+    
+    if (senhaInputValor2 != senhaInputValor){
+        senhaInputTexto.innerHTML = `<p class= "entrada-texto">Digite a mesma senha em ambos os campos de senha!<p>`
+
+        for(let i=0; i<= senhaInput.length -1; i++){
+            senhaInput[i].style.borderColor = 'var(--cor1)'  //pegas todos os inputs de senha e pinta a cor da borda
+        }
+    }
+
+    else {
+        senhaInputTexto.innerHTML = ''
+        for(let i=0; i<= senhaInput.length -1; i++){
+            senhaInput[i].style.borderColor = 'black'  //pegas todos os inputs de senha e pinta a cor da borda
+        }
+    }
+})
 
 
 
