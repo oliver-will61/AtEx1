eab12154de319b136c7bd65cfdb43efd324d80cd
Willian de Oliveira Ribeiro <123466324+oliver-will61@users.noreply.github.com>
2024-01-09 15:35:07 -0300

    add arquivo dados_doador.json

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,29 @@
-const doadores = document.querySelectorAll(".container-doador")
+const conteudoPrincipal = document.querySelector(".conteudo-principal");
 
-doadores.forEach((elemente) => elemente.addEventListener('click',  () => {
-    window.location.href = "pages/doador.html"
-}))
+fetch("../dados_doador.json") //lê o arquivo JSON
+    .then(response => response.json()
+    .then(dadosDoador => {
+
+        dadosDoador.forEach(element => {
+
+            conteudoPrincipal.innerHTML += `        
+            <div class="container-doador">
+                <img class="imagem-doador"src="${element.img}">
+
+                <div class="infomarcoes-doador">
+                    <h3 class="nome-doador">${element.nomeDoador}</h3>
+                    <p class="localização-doador">${element.localização}</p>
+                </div>
+            </div>`
+        });
+
+        const doadores = document.querySelectorAll(".container-doador")
+
+        doadores.forEach((elemente) => elemente.addEventListener('click',  () => {
+            window.location.href = "pages/doador.html"
+        }))
+
+    }))
+
+    .catch(erro => console.log("Erro ao carregar o arquivo JSON!"))
+
