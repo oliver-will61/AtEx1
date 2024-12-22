cd27c44fc7838e386bb903ed3d2b6987b3279b31
will <will.oliveira.2408@gmail.com>
2024-12-22 17:06:19 -0300

    alterençõs nas configs

--- applications/server.js/index.js
+++ applications/server.js/index.js
@@ -1,8 +1,28 @@
 //libs ========================================================================
 
 const express = require('express');
+const path = require('path') // Necessário para manipular caminhos de diretórios
+const mysql = require('mysql2/promise'); 
+
 const app = express();
-const mysql = require("mysql2/promise");
+const port = process.env.PORT || 58873 // porta do servidor
+
+// define o diretório público onde está o 'index.html'
+const publicDir = path.join(__dirname,'../../public_html')
+app.use(express.static(publicDir)); // Serve arquivos estáticos a partir do diretório definido
+
+//Rotas =====================================================================
+
+app.get('/', (req, res) => {
+    try {
+        res.sendFile(path.join(publicDir, 'index.html')); // Envia o arquivo `index.html`
+    } catch(error) {
+        console.error('Erro ao enviar o arquivo index.html:', error.message);
+        res.status(500).send('Erro interno no servidor');
+    }
+    
+})
+
 
 // Server ===================================================================
 
