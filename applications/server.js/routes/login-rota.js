47b765244e0a6517bbd8714c444f9881b4fd33df
Willian Ribeiro <will.oliveira2408@gmail.com>
2025-01-12 12:04:51 -0300

    add autenticacao de login

--- applications/server.js/routes/login-rota.js
+++ applications/server.js/routes/login-rota.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import {connectionPromise} from '../config/db.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
 app.use(express.json()) // Middleware para analisar JSON
+app.use(cookieParser()) // Middleware para ler cookies
 
 const router = express.Router() // Cria uma nova instância do roteador do Express para definir rotas separadas
-router.post('/login', async (req, res) => {
-    console.log('Requisição de login recebida');
 
+router.post('/login', async (req, res) => {
+
+    console.log('Requisição de login recebida');
 
     // Dados do login recebidos no body da requisição
     const emailLogin = req.body.email
@@ -19,6 +23,8 @@ router.post('/login', async (req, res) => {
 
         //Executa uma consulta SQL para selecionar o email e a senha da tabela 'usuarios'
         const [rows] = await connetion.execute('SELECT email, senha, id, nome FROM usuarios WHERE email = ?', [emailLogin])
+
+        // Usuario não encontrado =========================================================================================
 
         if (rows.length === 0){
             //email não encontrado no banco, o que significa que não foi cadastrado
@@ -33,7 +39,16 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({success: false, message: 'Senha incorreta'})
         }
 
-        // se o login for bem-sucedido 
+        //Usuario encontrado ===========================================================================================
+
+        const token = 'token_temporario' // Este seria o token JWT ou outro identificador, criptografar dps
+
+        res.cookie('auth_token', token, { // Envia o token de autenticação como cookie
+                httpOnly: true, // Não acessível via JavaScript (mais seguro)
+                secure: process.env.NODE_ENV === 'production', //Garantir que só seja enviado via HTTPS em produção
+                maxAge: 3600000 // tempo de expiração em milissegundos (1 hora)
+        });
+
         return res.status(200).json({
             success: true, 
             message: 'Login bem-sucedido',
@@ -45,7 +60,6 @@ router.post('/login', async (req, res) => {
         })
         
         
-
     } catch (error) {
         console.error('Erro ao processar o login', error);
         res.status(500).json({success: false, message: 'Erro interno ao servidor'})
